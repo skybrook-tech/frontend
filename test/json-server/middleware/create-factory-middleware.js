0afcc13524dea571a328a66bdbadbdc6e8a3a9c4
Kito Clark <kclark@bridj.com>
2020-01-24 07:56:19 +1000

    deleted json server related files

--- test/json-server/middleware/create-factory-middleware.js
+++
@@ -1,8 +0,0 @@
-const createFactoryMiddleware = factories => (req, res, next) => {
-  if (req.method === "POST") {
-    req.body = factories[req.path.replace("/", "")](req.body);
-  }
-  next();
-};
-
-module.exports = createFactoryMiddleware;
--- test/json-server/middleware/index.js
+++
@@ -1,4 +0,0 @@
-const auth = require("./auth");
-const createFactoryMiddleware = require("./create-factory-middleware");
-
-module.exports = { auth, createFactoryMiddleware };
